a4dd999fad7869db82dd180e99fc861607242baf
Mr. Mueller <bmueller@hstat.org>
2022-09-15 16:39:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// use document.write() to print "Hello World!"
+// use document.write() to print "Hello World!" (tip: put "Hello World!" inside the parentheses)
 
 
 // use console.log() to print "Testing the console..."
